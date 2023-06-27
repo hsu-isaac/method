5f21709e9f19399d07d498f54cb328296bcf42da
Isaac Hsu <16hsuisaac@gmail.com>
2023-06-27 00:10:56 -0700

    Set up upload and XML parser

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
+const multer = require("multer");
+const upload = multer();
+const xml2js = require ("xml2js");
+
 const app = express();
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.post("/api/uploadfile", upload.any() , async (req, res, next) => {
+  let xml = req.files[0].buffer.toString();
+  let xmlObj = await xml2js.parseStringPromise(xml);
+  console.log("Parsed XML:", JSON.stringify(xmlObj));
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
