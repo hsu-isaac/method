7dbc07d6023cee27d807e9d1d8ee6ba768d4c62b
Isaac Hsu <16hsuisaac@gmail.com>
2023-06-28 14:38:12 -0700

    moved all method interfacing functions to method-client file

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@
 import express from "express";
 import multer from "multer";
 import xml2js from "xml2js";
-import { createIndividualEntity } from "./method-client.js";
+import {
+        createIndividualEntity,
+        createCorporationEntity,
+        createSourceAccount,
+        findMerchantIdPlaid,
+        createLiabilityAccount,
+        createPayment
+        } from "./method-client.js";
 
 const PORT = process.env.PORT || 3001;
 const upload = multer();
@@ -25,60 +32,48 @@ app.post("/api/uploadfile", upload.any() , async (req, res, next) => {
   xmlJsonObj.root.row.forEach(async (payment) => {
 
     // Create individual entity
-    const entityIndividual = await createIndividualEntity(payment.Employee[0].FirstName[0], payment.Employee[0].LastName[0]);
-    console.log(entityIndividual)
+    const entityIndividual = await createIndividualEntity(
+      payment.Employee[0].FirstName[0],
+      payment.Employee[0].LastName[0]
+      );
 
     // Create corporation entity
-    console.log(payment.Payor[0].Address[0])
-    const entityCorporation = await method.entities.create({
-      type: 'llc',
-      corporation: {
-        name:   payment.Payor[0].Name[0],
-        dba:    payment.Payor[0].DBA[0],
-        ein:    payment.Payor[0].EIN[0],
-        owners: [],
-      },
-      address: {
-        line1:  payment.Payor[0].Address[0].Line1[0],
-        line2: (payment.Payor[0].Address[0].Line2 || [])[0],
-        city:   payment.Payor[0].Address[0].City[0],
-        state:  payment.Payor[0].Address[0].State[0],
-        zip:    payment.Payor[0].Address[0].Zip[0],
-      },
-    });
+    const entityCorporation = await createCorporationEntity(
+      payment.Payor[0].Name[0],
+      payment.Payor[0].DBA[0],
+      payment.Payor[0].EIN[0],
+      payment.Payor[0].Address[0].Line1[0],
+      payment.Payor[0].Address[0].City[0],
+      payment.Payor[0].Address[0].State[0],
+      payment.Payor[0].Address[0].Zip[0]
+      );
 
     // Create source account
-    const accountACH = await method.accounts.create({
-      holder_id: entityCorporation.id,
-      ach: {
-        routing: payment.Payor[0].ABARouting[0],
-        number:  payment.Payor[0].AccountNumber[0],
-        type:    'checking',
-      },
-    });
+    const accountACH = await createSourceAccount(
+      entityCorporation.id,
+      payment.Payor[0].ABARouting[0],
+      payment.Payor[0].AccountNumber[0]
+      );
 
     // Find merchant ID
-    const merchant = await method.merchants.list({"provider_id.plaid": payment.Payee[0].PlaidId[0]});
+    const merchant = await findMerchantIdPlaid(payment.Payee[0].PlaidId[0]);
 
     // Create liability account
-    const accountLiability = await method.accounts.create({
-      holder_id: entityIndividual.id,
-      liability: {
-        mch_id:         merchant[0].mch_id,
-        account_number: payment.Payee[0].LoanAccountNumber[0],
-      }
-    });
+    const accountLiability = await createLiabilityAccount(
+      entityIndividual.id,
+      merchant[0].mch_id,
+      payment.Payee[0].LoanAccountNumber[0]
+      );
 
     // Convert payment to cents
-    let paymentInCents = toCent(payment.Amount[0]);
+    const paymentInCents = toCent(payment.Amount[0]);
 
     // Create payment
-    const paymentInfo = await method.payments.create({
-      amount: paymentInCents,
-      source: accountACH.id,
-      destination: accountLiability.id,
-      description: 'Loan test',
-    });
+    const paymentInfo = await createPayment(
+      paymentInCents,
+      accountACH.id,
+      accountLiability.id
+    );
  });
 });
 
--- server/method-client.js
+++ server/method-client.js
@@ -17,4 +17,57 @@ export function createIndividualEntity (firstName, lastName) {
       phone: '+15121231111'
     }
   });
+};
+
+export function createCorporationEntity(name, dba, ein, address1, city, state, zip) {
+  return method.entities.create({
+    type: 'llc',
+    corporation: {
+      name: name,
+      dba: dba,
+      ein: ein,
+      owners: [],
+    },
+    address: {
+      line1: address1,
+      line2: [],
+      city: city,
+      state: state,
+      zip: zip,
+    }
+  });
+};
+
+export function createSourceAccount(corporationId, routing, number) {
+  return method.accounts.create({
+    holder_id: corporationId,
+        ach: {
+          routing: routing,
+          number: number,
+          type: 'checking',
+      }
+    });
+};
+
+export function findMerchantIdPlaid(plaidId) {
+  return method.merchants.list({"provider_id.plaid": plaidId});
+};
+
+export function createLiabilityAccount(individualId, merchantId, accountNumber) {
+  return method.accounts.create({
+    holder_id: individualId,
+    liability: {
+      mch_id: merchantId,
+      account_number: accountNumber,
+    }
+  });
+};
+
+export function createPayment(amount, source, destination) {
+  return method.payments.create({
+    amount: amount,
+    source: source,
+    destination: destination,
+    description: 'Loan test',
+  });
 }
